c0d1ee39b9574fc8fa4b28057e3fac290b4c0fe8
TangHao <524710389@qq.com>
2016-08-03 21:17:04 +0800

    [+]add setting panel data and handler.

---
+++ Project/Assets/Scripts/UserInterface/EditorSetting.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+using System.IO;
+using System.Xml;
+
+
+public class EditorSetting
+{
+    static readonly string sFilePath = Application.persistentDataPath + @"/EditorSettings.xml";
+
+    public static bool sDrawTiledFrame;
+
+
+
+    public static void Load()
+    {
+        if (File.Exists(sFilePath))
+        {
+            string tXmlContent = File.ReadAllText(sFilePath);
+            XmlDocument tDoc = new XmlDocument();
+            tDoc.LoadXml(tXmlContent);
+
+            sDrawTiledFrame = bool.Parse(tDoc.FirstChild.SelectSingleNode("DrawTiledFrame").Attributes["value"].Value);
+        }
+        else
+        {
+            Save(); // create if there is not one.
+        }
+    }
+
+    public static void Save()
+    {
+        XmlDocument tDoc = new XmlDocument();
+        XmlElement tRoot = tDoc.CreateElement("EdtiorSetting");
+
+        XmlElement tDrawTiledFrame = tDoc.CreateElement("DrawTiledFrame");
+        tDrawTiledFrame.SetAttribute("value", sDrawTiledFrame.ToString());
+        tRoot.AppendChild(tDrawTiledFrame);
+
+        tDoc.AppendChild(tRoot);
+
+        tDoc.Save(sFilePath);
+    }
+
+}
---
+++ Project/Assets/Scripts/UserInterface/EditorSettingHandler.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EditorSettingHandler : MonoBehaviour 
+{
+    public UIToggle mToggleDrawFrames;
+
+    public void onDrawTiledFrames()
+    {
+        EditorSetting.sDrawTiledFrame = mToggleDrawFrames.value;
+    }
+
+    public void onSave()
+    {
+        EditorSetting.Save();
+    }
+
+    public void onCancel()
+    {
+        EditorSetting.Load();
+    }
+
+    public void refreshSettings()
+    {
+        mToggleDrawFrames.value = EditorSetting.sDrawTiledFrame;
+    }
+	
+}
