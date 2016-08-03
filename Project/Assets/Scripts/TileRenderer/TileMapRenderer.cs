11a2d92c2a08cdce806fa231837004c54026ecc3
TangHao <524710389@qq.com>
2016-08-03 21:18:25 +0800

    [+]add tile render object. Pretext for rendering the tiled map.

---
+++ Project/Assets/Scripts/TileRenderer/TileMapRenderer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[DisallowMultipleComponent]
+public class TileMapRenderer : MonoBehaviour 
+{
+    TiledMap mTiledMap;
+
+	// Use this for initialization
+	void Start () 
+    {
+        GLDraw.Init();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnPostRender()
+    {
+        if (mTiledMap == null)
+        {
+            return;
+        }
+
+        // Draw tile frames.
+        GLDraw.DrawTiledFrames(0, 0, mTiledMap.mSizeX, mTiledMap.mSizeZ, 1, Color.green);
+
+        foreach (Tile tTile in mTiledMap.mTileList)
+        {
+            
+            
+        }
+    }
+}
